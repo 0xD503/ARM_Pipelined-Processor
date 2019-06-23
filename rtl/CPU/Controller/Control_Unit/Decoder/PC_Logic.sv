f35b919cf282cfda97cdc8c4f70a27e51ba6954e
EmbeddedDev <Antanta.TWWK@gmail.com>
2019-06-23 13:34:15 +0300

    Added PC logic module

---
+++ rtl/CPU/Controller/Control_Unit/Decoder/PC_Logic.sv
@@ -0,0 +1,11 @@
+module	ARM_Pipelined_PC_Logic
+	(input logic[3:0]	i_Rd,
+	input logic			i_Branch,
+	input logic			i_Reg_Write,
+	output logic		o_PC_Src_Decode);
+
+
+	assign o_PC_Source_Decode = ((i_Rd == 4'hF) & i_Reg_Write) ?	1'b1 : i_Branch;
+
+endmodule
+
